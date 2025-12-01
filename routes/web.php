9c92555da4d47ce1ab12c34b94b6ebe59ebb7627
Jerdon07 <jdlitaoen@gmail.com>
2025-12-01 15:33:09 +0800

    Removed the unneccessary frontend files

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Index', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -44,14 +44,6 @@ Route::get('/dashboard', function () {
 // --------------------------------------------------------
 // For All Users (Doesn't Requires checks)
 // --------------------------------------------------------
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
 Route::get('/crops', [CropController::class, 'index'])->name('crops');
 
