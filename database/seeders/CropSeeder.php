5d7a2754cce63676266062e69a0516932d1ee689
Jerdon07 <jdlitaoen@gmail.com>
2025-12-19 07:05:26 +0800

    refactor: Rename some table fields

--- database/seeders/CropSeeder.php
+++ database/seeders/CropSeeder.php
@@ -14,9 +14,9 @@ class CropSeeder extends Seeder
     public function run(): void
     {
         $crops = [
-            ['category_id' => 1, 'name' => 'Carrot', 'low_price' => 40, 'high_price' => 80, 'harvest_weeks' => 10],
-            ['category_id' => 1, 'name' => 'Cabbage', 'low_price' => 30, 'high_price' => 60, 'harvest_weeks' => 12],
-            ['category_id' => 2, 'name' => 'Banana', 'low_price' => 50, 'high_price' => 100, 'harvest_weeks' => 52],
+            ['category_id' => 1, 'name' => 'Carrot', 'price_min' => 40, 'price_max' => 80, 'harvest_weeks' => 10],
+            ['category_id' => 1, 'name' => 'Cabbage', 'price_min' => 30, 'price_max' => 60, 'harvest_weeks' => 12],
+            ['category_id' => 2, 'name' => 'Banana', 'price_min' => 50, 'price_max' => 100, 'harvest_weeks' => 52],
         ];
 
         foreach ($crops as $crop) {
