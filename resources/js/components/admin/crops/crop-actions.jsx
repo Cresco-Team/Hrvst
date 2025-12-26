8eec38b02681f70df2110e61a227b4a1623cd79b
Jerdon07 <jdlitaoen@gmail.com>
2025-12-26 22:24:57 +0800

    feat: Add crop actions for the crop table

---
+++ resources/js/components/admin/crops/crop-actions.jsx
@@ -0,0 +1,56 @@
+
+import { Link } from "@inertiajs/react"
+import { router } from "@inertiajs/react"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    MoreHorizontal,
+    ArrowUpDown,
+    ChartArea,
+    SquarePen,
+    Trash
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+export default function CropActions({ crop }) {
+    const handleDelete = () => {
+        if (!confirm("Delete this crop? This action is irreversible." - crop.name)) return
+
+        router.delete(route("crops.destroy", crop.id))
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem asChild>
+                    <Link
+                        onClick={() => router.visit(route("crops.edit", crop.id))}
+                    >
+                        <SquarePen />
+                        Edit
+                    </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem asChild>
+                    <Link
+                        onClick={handleDelete}
+                        className="flex text-destructive focus:text-destructive/70"
+                    >
+                        <Trash className="text-destructive" />
+                        Delete
+                    </Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
