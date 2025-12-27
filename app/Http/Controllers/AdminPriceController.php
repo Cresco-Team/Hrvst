1bda61b12b04ede947ef7bb959e06cc5fe4ec2d7
Jerdon07 <jdlitaoen@gmail.com>
2025-12-27 18:29:53 +0800

    feat: Add index method

--- app/Http/Controllers/AdminPriceController.php
+++ app/Http/Controllers/AdminPriceController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Crop;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AdminPriceController extends Controller
 {
-    //
+    public function index()
+    {
+        $crops = Crop::with([
+            'category',
+            'latestPrice:id,crop_id,price_min,price_max,recorded_at'
+        ])->get();
+        
+        return Inertia::render('admin/prices/index', [
+            'crops' => $crops,
+        ]);
+    }
 }
