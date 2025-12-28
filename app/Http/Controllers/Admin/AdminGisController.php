af0a7ca9410090c5c0e9add9d1389cd70e559f9e
Jerdon07 <jdlitaoen@gmail.com>
2025-12-28 18:30:10 +0800

    feat: Fetch the farmers data to inertia

--- app/Http/Controllers/Admin/AdminGisController.php
+++ app/Http/Controllers/Admin/AdminGisController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Farmer;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +11,14 @@ class AdminGisController extends Controller
 {
     public function index()
     {
-        return Inertia::render('admin/farmers/geolocation/index');
+        $farmers = Farmer::with([
+            'user',
+            'municipality',
+            'barangay'
+        ])->get();
+
+        return Inertia::render('admin/farmers/geolocation/index',[
+            'farmers' => $farmers,
+        ]);
     }
 }
