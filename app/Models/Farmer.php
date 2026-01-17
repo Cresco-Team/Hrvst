fb8a73ced94e3156e117e5c14b8cc238bf7b075d
Jerdon07 <jdlitaoen@gmail.com>
2026-01-17 19:14:01 +0800

    refactor: Add municipality and barangay relationships

--- app/Models/Farmer.php
+++ app/Models/Farmer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Farmer extends Model
 {
@@ -21,30 +22,40 @@ class Farmer extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function municipality(): BelongsTo
+    {
+        return $this->belongsTo(Municipality::class);
+    }
+
+    public function barangay(): BelongsTo
+    {
+        return $this->belongsTo(Barangay::class);
+    }
+
     /* 
         Use scope method to only include approved farmers
         When calling the scope, you omit the 'scope' prefix
         scopeApproved will be 'approved'
     */
-    /* public function scopeApproved(Builder $query): Builder
+    public function scopeApproved(Builder $query): Builder
     {
         return $query->whereHas('user', function ($q) {
             $q->where('isApproved', 1);
         });
-    } */
+    }
 
-    /* public function scopePending(Builder $query) : Builder
+    public function scopePending(Builder $query) : Builder
     {
         return $query->whereHas('user', function ($q) {
             $q->where('isApproved', 0);
         });
-    } */
+    }
 
-    /* public function crops()
+    public function crops()
     {
         return $this->belongsToMany(Crop::class, 'farmer_crop')
                     ->using(FarmerCrop::class)
                     ->withPivot(['yield_kg', 'date_planted', 'date_harvested'])
                     ->withTimestamps();
-    } */
+    }
 }
