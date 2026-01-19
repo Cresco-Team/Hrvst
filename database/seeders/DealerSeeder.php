828a2f059014243de6ac68f50c5553e1aabd7226
Jerdon07 <jdlitaoen@gmail.com>
2026-01-19 19:56:21 +0800

    refactor: Add seeder for a dealer user

--- database/seeders/DealerSeeder.php
+++ database/seeders/DealerSeeder.php
@@ -2,13 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dealer;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DealerSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $dealerRole = Role::firstOrCreate([
+            'name' => 'dealer'
+        ]);
+
+        $user = User::firstOrCreate([
+            'email' => 'dealer@hrvst.com',
+            'phone_number' => '364435545'
+        ], [
+            'name' => 'Dealer Joe',
+            'password' => Hash::make('password'),
+            'isApproved' => true,
+        ]);
+
+        $user->roles()->syncWithoutDetaching([$dealerRole->id]);
     }
 }
