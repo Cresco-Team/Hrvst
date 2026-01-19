09ed08a9293d867267853fff4000a10842271bd4
Jerdon07 <jdlitaoen@gmail.com>
2026-01-19 19:57:20 +0800

    refactor: Remove the pending page and Move the auth admin route from auth.php to the web.php

--- routes/auth.php
+++ routes/auth.php
@@ -65,41 +65,6 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
-
-    Route::get('/pending', function () {
-        if (Auth::user()->isApproved) {
-            return redirect()->route('crops.index');
-        }
-        return Inertia::render('Auth/Pending');
-    })->name('pending');
 });
 
-Route::middleware(['auth', 'admin'])->prefix('admin')->as('admin.')->group(function () {
 
-    /* Hero */
-    Route::get('/', function () {
-        return Inertia::render('admin/index');
-    })->name('index');
-
-    /* Dashboard Overview */
-    Route::get('/dashboard', AdminDashboardController::class)->name('dashboard');
-
-    /* Vegetable Spreadsheet */
-    Route::resource('crops', AdminCropController::class);
-    Route::resource('crops.prices', AdminPriceController::class)->only(['create', 'store']);
-
-    Route::get('/prices', [AdminPriceController::class, 'index'])->name('prices.index');
-
-    Route::get('/price-trends', PriceTrends::class)->name('price-trends');
-    
-    /* Farmers Dashboard */
-    Route::get('farmers', [AdminFarmerController::class, 'index'])->name('farmers.index');
-    // No farmers create and store
-    Route::get('/farmers/pending/{farmer}', [AdminFarmerController::class, 'show'])->name('farmers.show');
-    // No farmers edit and update
-    Route::post('/farmers/pending/{user}/approve', [AdminFarmerController::class, 'approve'])->name('farmers.approve'); // Approve Pending Farmers
-    Route::delete('/farmers/pending/{user}/delete', [AdminFarmerController::class, 'delete'])->name('farmers.delete');    // Reject Pending Farmers
-
-    Route::get('geolocation', [AdminGisController::class, 'index'])->name('gis.index');
-    Route::get('demographics', [AdminDemoController::class, 'index'])->name('demo.index');
-});
--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,59 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminCropController;
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\AdminDemoController;
 use App\Http\Controllers\Admin\AdminFarmerController;
+use App\Http\Controllers\Admin\AdminGisController;
+use App\Http\Controllers\Admin\AdminPriceController;
 use App\Http\Controllers\CropController;
 use App\Http\Controllers\FarmerController;
 use App\Http\Controllers\FarmerProfileController;
+use App\Http\Controllers\PriceTrends;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// --------------------------------------------------------
-// Public API routes
-// --------------------------------------------------------
+/* Home Page */
 Route::get('/', function () {
     return Inertia::render('index', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 })->name('home');
 
+/* Default Pages */
 Route::get('/farmers', [FarmerController::class, 'index'])->name('farmers.index');
-
 Route::get('/crops', [CropController::class, 'index'])->name('crops.index');
 Route::get('crops/{crop}', [CropController::class, 'show'])->name('crops.show');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/pending', function() {
-        if (Auth::user()->isApproved) {
-            return redirect()->route('farmers.index');
-        } return Inertia::render('index');
-    })->name('pending');
-});
+// --------------------------------------------------------
+// Admin Only Page
+// --------------------------------------------------------
+Route::middleware(['auth', 'admin'])->prefix('admin')->as('admin.')->group(function () {
 
-// --------------------------------------------------------
-// Auth Routes for Farmers
-// --------------------------------------------------------
-Route::middleware(['approved.farmer'])->group(function () {
-    Route::get('/profile', [FarmerProfileController::class, 'show'])->name('profile.edit');
-    Route::patch('/profile', [FarmerProfileController::class, 'update'])->name('profile.update');
+    Route::get('/dashboard', AdminDashboardController::class)->name('dashboard');
+
+    /* Vegetable Spreadsheet */
+    Route::resource('crops', AdminCropController::class);
+    Route::resource('crops.prices', AdminPriceController::class)->only(['create', 'store']);
+
+    Route::get('/prices', [AdminPriceController::class, 'index'])->name('prices.index');
+
+    Route::get('/price-trends', PriceTrends::class)->name('price-trends');
+    
+    /* Farmers Dashboard */
+    Route::get('farmers', [AdminFarmerController::class, 'index'])->name('farmers.index');
+    // No farmers create and store
+    Route::get('/farmers/pending/{farmer}', [AdminFarmerController::class, 'show'])->name('farmers.show');
+    // No farmers edit and update
+    Route::post('/farmers/pending/{user}/approve', [AdminFarmerController::class, 'approve'])->name('farmers.approve'); // Approve Pending Farmers
+    Route::delete('/farmers/pending/{user}/delete', [AdminFarmerController::class, 'delete'])->name('farmers.delete');    // Reject Pending Farmers
+
+    Route::get('geolocation', [AdminGisController::class, 'index'])->name('gis.index');
+    Route::get('demographics', [AdminDemoController::class, 'index'])->name('demo.index');
 });
 
 require __DIR__.'/auth.php';
