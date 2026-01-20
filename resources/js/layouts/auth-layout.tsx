c557f1bb6cb10ee3509204c8b6e4df25bcb17d61
Jerdon07 <jdlitaoen@gmail.com>
2026-01-20 15:14:38 +0800

    refactor: Add layout for all authenticated users

---
+++ resources/js/layouts/auth-layout.tsx
@@ -0,0 +1,46 @@
+import AuthSidebar from '@/components/sidebars/auth-sidebar';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { Head } from '@inertiajs/react';
+
+const AuthLayout = ({ children, title }) => {
+    return (
+        <SidebarProvider className="h-dvh">
+            <Head title={title} />
+
+            <AuthSidebar />
+
+            <SidebarInset className="overflow-hidden">
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 data-[orientation=vertical]:h-4"
+                        />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem className="hidden md:block">
+                                    <BreadcrumbLink href="#">
+                                        Building Your Application
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator className="hidden md:block" />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>
+                                        Data Fetching
+                                    </BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
+                <Separator />
+
+                <main className="flex-1 p-4">{children}</main>
+            </SidebarInset>
+        </SidebarProvider>
+    );
+};
+export default AuthLayout;
