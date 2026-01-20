49d3e4c32b43cfbcfb9fcd5ad7654f1cd919982e
Jerdon07 <jdlitaoen@gmail.com>
2026-01-20 20:24:05 +0800

    refactor: Restyle the auth layout

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -6,12 +6,12 @@ import { Head } from '@inertiajs/react';
 
 const AuthLayout = ({ children, title }) => {
     return (
-        <SidebarProvider className="h-dvh">
+        <SidebarProvider className="h-full overflow-hidden flex">
             <Head title={title} />
 
             <AuthSidebar />
 
-            <SidebarInset className="h-screen flex flex-col overflow-hidden">
+            <SidebarInset className="flex flex-col flex-1 overflow-hidden">
                 <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
