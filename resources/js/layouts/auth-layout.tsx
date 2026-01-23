78bc0bd42792605c8c6de234102c01b9af0c92dd
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-23 16:27:54 +0800

    REFACTOR: Make the breadcrumb dynamic

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -4,7 +4,7 @@ import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Head } from '@inertiajs/react';
 
-const AuthLayout = ({ children, title }) => {
+const AuthLayout = ({ children, title, breadcrumbs = [] }) => {
     return (
         <SidebarProvider className="h-full overflow-hidden flex">
             <Head title={title} />
@@ -13,33 +13,20 @@ const AuthLayout = ({ children, title }) => {
 
             <SidebarInset className="flex flex-col flex-1 overflow-hidden">
                 <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"
-                        />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>
-                                        Data Fetching
-                                    </BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="h4" />
+
+                    {breadcrumbs.length > 0 && (
+                        <Breadcrumb className='hidden md:flex'>
+                        
                         </Breadcrumb>
-                    </div>
+                    )}
                 </header>
+
                 <Separator />
 
                 <main className="flex-1 overflow-y-auto">
-                    <div className='flex flex-col gap-4 p-4 pt-0'>
+                    <div className='flex flex-col gap-4 p-4'>
                         {children}
                     </div>
                 </main>
