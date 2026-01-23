0dc480a231899428544a824b80a1e0a3440e7db8
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-23 18:42:26 +0800

    STYLE: SidebarInset UI

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -12,8 +12,8 @@ const AuthLayout = ({ children, title, breadcrumbs = [] }) => {
             <AuthSidebar />
 
             <SidebarInset className="flex flex-col flex-1 overflow-hidden">
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <SidebarTrigger className="-ml-1" />
+                <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+                    <SidebarTrigger className="size-9" />
                     <Separator orientation="vertical" className="h4" />
 
                     {breadcrumbs.length > 0 && (
