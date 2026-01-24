78b898d7ec3351e1901817725e209aa6444a80f6
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-24 22:32:01 +0800

    REFACTOR: Remove profile pages

--- resources/js/services/AuthPages/farmer-pages.ts
+++ resources/js/services/AuthPages/farmer-pages.ts
@@ -5,10 +5,6 @@ const farmerPages = [
         title: 'Main',
         pages: [
             {
-                title: 'My Profile',
-                url : 'farmer.show',
-                icon: CircleUserRoundIcon
-            }, {
                 title: 'Garden',
                 url : 'farmer.plantings.index',
                 icon: SproutIcon
