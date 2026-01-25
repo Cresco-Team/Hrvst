da4f1f55d32e06a6a7026d40beabb21f2c4156fa
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-25 17:01:24 +0800

    REFACTOR: Include planting.id in the onContactFarmer

--- resources/js/components/features/dealer/marketplace/planting-card.jsx
+++ resources/js/components/features/dealer/marketplace/planting-card.jsx
@@ -100,7 +100,7 @@ const PlantingCard = ({ planting, onContactFarmer }) => {
 
             <CardFooter>
                 <Button 
-                    onClick={() => onContactFarmer(planting.farmer.id)} 
+                    onClick={() => onContactFarmer(planting.farmer.id, planting.id)} 
                     className="w-full"
                 >
                     <MessageSquareIcon className="mr-2 h-4 w-4" />
