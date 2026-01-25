e9bef28fed2dfd66877a39a4d9d69de85e642152
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-25 10:34:48 +0800

    FEAT: Add message repository to handle the conversation content

---
+++ app/Repositories/MessageRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Message;
+
+class MessageRepository
+{
+    public function create(array $data): Message
+    {
+        return Message::create($data);
+    }
+
+    public function markAsRead(int $messageId): void
+    {
+        Message::where('id', $messageId)
+            ->whereNull('read_at')
+            ->update(['read_at' => now()]);
+    }
+
+    public function markConversationAsRead(int $conversationId, int $userId): void
+    {
+        Message::where('conversation_id', $conversationId)
+            ->where('sender_id', '!=', $userId)
+            ->whereNull('read_at')
+            ->update(['read_at' => now()]);
+    }
+}
