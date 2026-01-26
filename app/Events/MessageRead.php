620d9ef8affccc7ba559498a16ea605e14c8a15a
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-26 23:35:33 +0800

    REFACTOR: Create MessageRead event (real-time read receipts)

---
+++ app/Events/MessageRead.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageRead
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public int $conversationId,
+        public int $userId,
+        public string $timestamp,
+    )
+    {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('conversation.' . $this->conversationId),
+        ];
+    }
+
+    public function broadcastas(): string
+    {
+        return 'message.read';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'user_id' => $this->userId,
+            'read_at' => $this->timestamp,
+        ];
+    }
+}
