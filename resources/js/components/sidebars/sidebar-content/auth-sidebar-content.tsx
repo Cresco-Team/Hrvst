2b7f5cec4e0f15fa5525785c01093aa3d76c31dc
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-28 00:10:23 +0800

    REFACTOR: Add public pages to app-sidebar-content

--- resources/js/components/sidebars/sidebar-content/auth-sidebar-content.tsx
+++ resources/js/components/sidebars/sidebar-content/auth-sidebar-content.tsx
@@ -1,12 +1,36 @@
+import publicPages from "@/services/AuthPages/public-pages"
 import { SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import { Link } from "@inertiajs/react"
 
 
-const AuthSidebarContent = ({ sidebarContents }) => {
+const AuthSidebarContent = ({ authContents }) => {
 
     return (
         <SidebarContent className="gap-0">
-              {sidebarContents?.map(group => (
+              {publicPages?.map(group => (
+                <SidebarGroup key={group.title} className="py-1">
+                  <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                      <SidebarMenu>
+                        {group.pages.map((page: any) => (
+                          <SidebarMenuItem key={page.url}>
+                          <SidebarMenuButton
+                            asChild
+                            isActive={route().current(page.url)}
+                          >
+                            <Link href={route(page.url)}>
+                              <page.icon />
+                              {page.title}
+                            </Link>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                        ))}
+                      </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+              ))}
+
+              {authContents?.map(group => (
                 <SidebarGroup key={group.title} className="py-1">
                   <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
                     <SidebarGroupContent>
