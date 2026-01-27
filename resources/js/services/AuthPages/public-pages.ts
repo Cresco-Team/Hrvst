c7127e3624315d8e60620b19ea00e4c56fd99e36
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-27 23:34:55 +0800

    FEAT: Add public pages url service

---
+++ resources/js/services/AuthPages/public-pages.ts
@@ -0,0 +1,20 @@
+import { MapPinnedIcon, SproutIcon } from "lucide-react";
+
+
+const publicPages = [
+    {
+        title: 'Main',
+        pages: [
+            {
+                title: 'Vegetables',
+                url: 'crops',
+                icon: SproutIcon,
+            }, {
+                title: 'Farmers',
+                url: 'farmers',
+                icon: MapPinnedIcon,
+            }
+        ]
+    }
+]
+export default publicPages
